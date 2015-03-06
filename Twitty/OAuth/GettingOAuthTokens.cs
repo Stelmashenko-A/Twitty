a9a3644d7ea9088aa3b209c12800bb012e9a15bd
Stelmashenko-A <stelma_28@bk.ru>
2015-03-06 11:46:01 +0300

    Создание класса GettingOAuthTokens
    Класс хранит токены получаемые от Twitter после соответствующего запроса

---
+++ Twitty/OAuth/GettingOAuthTokens.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Twitty.OAuth
+{
+    class GettingOAuthTokens
+    {
+        public string Token { get; set; }
+
+        public string TokenSecret { get; set; }
+
+        public string ScreenName { get; set; }
+
+        public decimal UserId { get; set; }
+
+        public string VerificationString { get; set; }
+    }
+}
