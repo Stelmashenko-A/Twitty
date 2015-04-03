ab407779e0049926a3086a65c3d0bfaa259da4b1
Stelmashenko-A <stelma_28@bk.ru>
2015-04-03 10:51:17 +0300

    Создание класса Hashtags

---
+++ Twitty/Entities/Hashtags.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Twitty.Entities
+{
+    class Hashtags
+    {
+        public List<int> Indices { get; set; }
+        public String Text { get; set; }
+    }
+}
