87cf77e1d801ce8b36f12169000df3ede6fabdb5
Stelmashenko-A <stelma_28@bk.ru>
2015-04-03 11:16:30 +0300

    Создание класса Media

---
+++ Twitty/Entities/Media.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Twitty.Entities
+{
+    class Media
+    {
+        public String DisplayUrl { get; set; }
+        public String ExpandedUrl { get; set; }
+        public Int64 Id { get; set; }
+        public String IdStr { get; set; }
+        public List<int> Indices { get; set; }
+        public String MediaUrl { get; set; }
+        public String MediaUrlHtps { get; set; }
+        //public Size Sizes{get;set;}
+        public Int64 SourceStatusId { get; set; }
+        public String Type { get; set; }
+        public String Url { get; set; }
+ 
+    }
+}
