e57ad1817cc7d40cadff5a42f88fcc9cfedca065
Stelmashenko-A <stelma_28@bk.ru>
2015-04-03 13:08:10 +0300

    Создание класса TwitterUrl

---
+++ Twitty/Entities/TwitterUrl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Twitty.Entities
+{
+    class TwitterUrl
+    {
+        public String DisplayUrl { get; set; }
+        public String ExpandedUrl { get; set; }
+        public List<int> Indices { get; set; }
+        public String UrlStr { get; set; }
+
+    }
+}
