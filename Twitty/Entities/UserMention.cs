baba077bc3a5b9febb288f0c58341324bfd7a7a6
Stelmashenko-A <stelma_28@bk.ru>
2015-04-03 13:11:44 +0300

    Создание класса UserMention

---
+++ Twitty/Entities/UserMention.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Twitty.Entities
+{
+    class UserMention
+    {
+        public Int64 Id { get; set; }
+        public String IdStr { get; set; }
+        public List<int> Indices { get; set; }
+        public String Name { get; set; }
+        public String ScreenName { get; set; }
+    }
+}
